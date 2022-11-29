156e18d5aaec1af6f1876e6729abd7e91f361360
407 <407@academicos.cesde.edu.co>
2022-11-29 16:38:41 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,76 @@
+import './Home.css'
+export function Home(){
+    return(
+        <>
+            <section>
+              <div class="banner">
+                
+              </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h2>The Neighbourhood</h2>
+                            <p>The Neighbourhood (abreviado como THE NBHD) es una banda de musica alternativa rock estadounidense creada en agosto de 2011. El grupo está formado por el vocalista Jesse Rutherford, los guitarristas Jeremy Freedman, Zach Abels y el bajista Mikey Margott. El baterista Brandon Fried fue expulsado de la banda el 13 de noviembre del 2022 tras ser denunciado por abuso sexual públicamente por Maria Zardoya, integrante de la banda de The Marias. La banda publicó su primer álbum de estudio el 23 de abril de 2013 a través de Columbia Records. El 16 de enero de 2014 la banda anunció a través de sus redes sociales que el baterista Bryan Sammis abandonaba el grupo por problemas con el vocalista Jesse Rutherford, y que continuaría su carrera musical en solitario. En noviembre de 2014 lanzaron los álbumesThank you, I Love You, The Love Collection, Wiped Out! y Hard EP, que destacan por sus portadas en blanco y negro. En 2018, lanzaron su primer álbum con una portada en color, el cual cuenta con un sonido mucho más electrónico que sus predecesores y con el que obtuvieron más reconocimiento. Su álbum más reciente es Chip Chrome & The Mono-Tones.</p>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjegc.appspot.com/o/The-Neighbourhood-concierto.webp?alt=media&token=9314b056-1c6e-4ac9-a72c-d752ee6fffd3" class="w-100"></img>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="row">
+                                <h2 class="text-center">Conciertos</h2>
+                                <div class="col-6"><i class="bi bi-cassette-fill fs-1"></i></div>
+                                <div class="col-6 fs-1">
+                                    <h3>Med</h3>
+                                    <h5>Dec 06</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><i class="bi bi-cassette-fill fs-1"></i></div>
+                                <div class="col-6 fs-1">
+                                    <h3>Med</h3>
+                                    <h5>Dec 06</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><i class="bi bi-cassette-fill fs-1"></i></div>
+                                <div class="col-6 fs-1">
+                                    <h3>Med</h3>
+                                    <h5>Dec 06</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><i class="bi bi-cassette-fill fs-1"></i></div>
+                                <div class="col-6 fs-1">
+                                    <h3>Med</h3>
+                                    <h5>Dec 06</h5>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjegc.appspot.com/o/carrucel%201.jpg?alt=media&token=0c959b59-87b0-47ec-9741-810af9474bf0" alt="" class="img-fluid w-100"/>
+
+
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjegc.appspot.com/o/carrucel%202.jpg?alt=media&token=b518872c-f306-4549-9e76-9ce02e2e1471" alt="" class="img-fluid w-100"/>
+
+
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjegc.appspot.com/o/carrucel%203.png?alt=media&token=26d8acc2-1273-477a-aaf9-148cd3536bfd" alt="" class="img-fluid w-100"/>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
